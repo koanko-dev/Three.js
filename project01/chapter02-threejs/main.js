f93c1c5963767876d7deb00b4dd0e39008a0dcce
Jihye An <koanko.dev@gmail.com>
2024-03-15 17:58:00 +0900

    test controls

--- project01/chapter02-threejs/main.js
+++ project01/chapter02-threejs/main.js
@@ -1,6 +1,10 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FlyControls } from "three/examples/jsm/controls/FlyControls";
+import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.shadowMap.enabled = true;
@@ -56,9 +60,67 @@ const directionalLightHelper = new THREE.DirectionalLightHelper(
 );
 scene.add(directionalLightHelper);
 
-// 화면을 컨트롤 할 수 있도록 만들기
-const orbitControls = new OrbitControls(camera, renderer.domElement);
-orbitControls.update();
+// // OrbitControls
+// const orbitControls = new OrbitControls(camera, renderer.domElement);
+// orbitControls.enableDamping = true; // default = false
+// orbitControls.dampingFactor = 0.03; // default =  0.05. 작아질수록 오래 유지.
+// orbitControls.enableZoom = true; // 줌 가능하도록. default = true
+// orbitControls.enablePan = true; // 카메라의 위치 변경할 수 있도록. default = true
+// orbitControls.enableRotate = true; // 카메라 회전할 수 있도록. default = true
+// orbitControls.autoRotate = false; // 자동 회전하도록. default = false
+// orbitControls.autoRotateSpeed = 1; // default = 2. 클수록 회전 속도 빠름
+// // 드래그를 이용한 회전반경 조절
+// orbitControls.maxPolarAngle = Math.PI / 2; // 수직방향으로 회전 가능한 각도. 끝점
+// orbitControls.minPolarAngle = Math.PI / 4; // 수직방향으로 회전 시작할 각도. 시작점
+// orbitControls.maxAzimuthAngle = Math.PI / 2; // 수평방향으로 회전 가능한 각도. 끝점
+// orbitControls.minAzimuthAngle = Math.PI / -2; // 수평방향으로 회전 시작할 각도. 시작점
+
+// // FlyControls
+// const flyControls = new FlyControls(camera, renderer.domElement);
+// flyControls.movementSpeed = 10; // wasd 키로 움직일때 카메라 이동속도
+// flyControls.rollSpeed = Math.PI / 4; // 마우스 움질일 때 카메라의 회전 속도
+
+// // FirstPersonControls
+// camera.position.set(0, 1, 5)
+// const firstPersonControls = new FirstPersonControls(
+//   camera,
+//   renderer.domElement
+// );
+// firstPersonControls.lookSpeed = 0.1; // 카메라의 회전 속도
+// firstPersonControls.movementSpeed = 1; // 카메라의 이동 속도
+// firstPersonControls.lookVertical = false; // 카메라 수직 이동 활성화. default = true
+
+// const clock = new THREE.Clock();
+
+// // PointerLockControls
+// camera.position.set(0, 1, 5)
+// const pointerLockControls = new PointerLockControls(
+//   camera,
+//   renderer.domElement
+// );
+// window.addEventListener("click", () => {
+//   pointerLockControls.lock();
+// });
+
+// TrackballControls
+const trackballControls = new TrackballControls(camera, renderer.domElement);
+trackballControls.rotateSpeed = 2; // 회전하는 속도
+trackballControls.zoomSpeed = 1.5; // 줌하는 속도
+trackballControls.panSpeed = 0.5; // 회전하는 속도
+trackballControls.noRotate = false; // 회전 허용F
+trackballControls.noZoom = false; // 줌 허용
+trackballControls.noPan = false; // 카메라 회전 허용
+trackballControls.staticMoving = false; // 댐핑이 없는 움직임을 활성할지에 대한 여부
+trackballControls.dynamicDampingFactor = 0.05; // 댐핑의 정도
+
+// trackball의 타겟을 바꾸고 싶다면,
+const target = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5),
+  new THREE.MeshStandardMaterial({ color: 0x0000ff })
+);
+target.position.set(4, 0.5, 0);
+scene.add(target);
+trackballControls.target = target.position;
 
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -71,6 +133,10 @@ window.addEventListener("resize", () => {
 const render = () => {
   renderer.render(scene, camera);
   requestAnimationFrame(render);
+  // orbitControls.update();
+  // flyControls.update(clock.getDelta());
+  // firstPersonControls.update(clock.getDelta());
+  trackballControls.update();
 };
 
 render();
