7ac1a1f3a962cef962960501c758ef32b2db1fd7
Jihye An <koanko.dev@gmail.com>
2024-03-16 10:51:52 +0900

    test pointer event

--- project01/chapter02-threejs/main.js
+++ project01/chapter02-threejs/main.js
@@ -28,6 +28,7 @@ const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 floor.rotation.x = -Math.PI / 2;
 floor.receiveShadow = true;
 floor.castShadow = true;
+floor.name = "FLOOR";
 scene.add(floor);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
@@ -62,31 +63,30 @@ scene.add(character);
 const mixer = new THREE.AnimationMixer(character);
 const animationClips = data.animations;
 const action = mixer.clipAction(animationClips[3]);
-// action.setLoop(THREE.LoopOnce); // 한번만 실행
-action.setLoop(THREE.LoopRepeat); // default. 계속해서 반복해 실행
-// action.setLoop(THREE.LoopPingPong); // 끝까지 실행되면 역재생되어 다시 돌아오는 걸 반복
-// action.setDuration(10); // 애니메이션 속력
-// action.setEffectiveTimeScale(2); // 애니메이션 속력 비율. 몇배속
-// action.setEffectiveWeight(0.5); // 움직임을 정도. 춤이라면 덜 추거나 더 추거나를 조절
+action.setLoop(THREE.LoopPingPong); // 끝까지 실행되면 역재생되어 다시 돌아오는 걸 반복
 action.play();
-
-// 애니메이션 멈추기
-setTimeout(() => {
-  action.paused = true;
-}, 3000);
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.enableDamping = true;
 orbitControls.dampingFactor = 0.03;
-orbitControls.enableZoom = true;
-orbitControls.enablePan = true;
-orbitControls.enableRotate = true;
-orbitControls.autoRotate = false;
-orbitControls.autoRotateSpeed = 1;
-orbitControls.maxPolarAngle = Math.PI / 2;
-orbitControls.minPolarAngle = Math.PI / 4;
-orbitControls.maxAzimuthAngle = Math.PI / 2;
-orbitControls.minAzimuthAngle = Math.PI / -2;
+
+const newPosition = new THREE.Vector3(0, 1, 0);
+const rayCaster = new THREE.Raycaster();
+
+renderer.domElement.addEventListener("pointerdown", (e) => {
+  const x = (e.clientX / window.innerWidth) * 2 - 1;
+  const y = -((e.clientY / window.innerHeight) * 2 - 1);
+  // rayCaster 스크린에 쏠 준비
+  rayCaster.setFromCamera(new THREE.Vector2(x, y), camera);
+  // scene에 있는 children을 한정으로 관통을 하겠다는 의미
+  const intersects = rayCaster.intersectObjects(scene.children);
+
+  const intersectFloor = intersects.find((i) => i.object.name === "FLOOR");
+  console.log("interesctFloor", intersectFloor);
+  // copy: 파라미터 값을 복사해서 넣어주겠다는 의미
+  newPosition.copy(intersectFloor.point);
+  newPosition.y = 1;
+});
 
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -96,9 +96,27 @@ window.addEventListener("resize", () => {
 });
 
 const clock = new THREE.Clock();
+const targetVector = new THREE.Vector3();
 
-// requestAnimationFrame 사용
 const render = () => {
+  // 캐릭터가 클릭한 곳을 보도록 설정
+  character.lookAt(newPosition);
+  // 경로 생성
+  targetVector
+    .subVectors(newPosition, character.position) // newPosition과 캐릭터의 position을 뺀 벡터를 할당해주고
+    .normalize() // 그 벡터를 정규화(1로 만들어주는 작업)를 한다.
+    .multiplyScalar(0.01); // 방향은 바꾸지 않고 크기만 0.01배
+  // 경로대로 움직이게 만들기
+  if (
+    Math.abs(character.position.x - newPosition.x) >= 1 ||
+    Math.abs(character.position.z - newPosition.z) >= 1
+  ) {
+    character.position.x += targetVector.x;
+    character.position.z += targetVector.z;
+    action.stop();
+  }
+  action.play();
+
   renderer.render(scene, camera);
   requestAnimationFrame(render);
   orbitControls.update();
